925e4a59726d22362fa9f27e64d23298fb68ec45
ReadyToRoll <cayman335@gmail.com>
2014-03-11 20:51:46 +0300

    new commit

--- src/hybrid_stack/Mass_stack.java
+++ src/hybrid_stack/Mass_stack.java
@@ -5,22 +5,22 @@ import java.util.Scanner;
 
 class Mass_stack
 {
-	public int arr[];
-	public Mass_stack array[];
+	public int arr[][];
 	static int index=0;
 	private final int maxsize=100;
 	Mass_stack()
 	{
-		array=new Mass_stack[maxsize];
+		arr=new int[maxsize][maxsize];
 	}
 	public void push(int val[])
 	{
-		if(index>maxsize){
+		if(index>maxsize)
 			System.out.println("Stack overflow! ");
-		}
 		else
 		{
-			array[index].arr=val; //Почему мы не можем так написать?
+			arr[index][0]=val.length+1;
+			for(int i=1;i<=val.length;i++)
+			arr[index][i]=val[i-1];
 			index++;
 		}
 	}
@@ -28,37 +28,32 @@ class Mass_stack
 	{
 		if(index!=0)
 		{
+			for(int i=0;i<maxsize;i++)
+				arr[index][i]=0;
 			index--;
-			//int temp[]=array[index].arr;
-		//	return temp[];
 		}
 		else
-		{
 			System.out.println("Stack is empty!");
-			//return -111;
-		}
 	}
 	public void show()
 	{
 		if(index!=0)
 		{
-			System.out.print("Элементы стека: ");
+			System.out.println("Элементы стека: ");
 			for(int i=0;i<index;i++)
 			{
-				System.out.print("Массив №"+i+":");
-				for(int j=0;j<arr.length;j++)
-					System.out.print(array[i].arr[j]+" "); //Аналогично и это не работает..
+				System.out.print("Массив №"+(i+1)+":");
+				for(int j=0;j<arr[i][0];j++)
+					System.out.print(arr[i][j]+" "); //Аналогично и это не работает..
 				System.out.println();
 			}
 		}
 		else
-		{
 			System.out.println("Стек пуст!");
-		}
 	}
 	public static void main(String[] args)
 	{
-		Mass_stack me=new Mass_stack();
+		Mass_stack test=new Mass_stack();
 		int n=3;
 		int val1[];
 		val1=new int[n];
@@ -69,20 +64,20 @@ class Mass_stack
 		Scanner inp = new Scanner(System.in);
 		System.out.print("Введите "+n+" значений для ввода в стек: ");
 		for(int i=0;i<n;i++)
-		val1[i] = inp.nextInt();
+			val1[i] = inp.nextInt();
 		System.out.print("Введите "+n+" значений для ввода в стек: ");
 		for(int i=0;i<n;i++)
-		val2[i] = inp.nextInt();
+			val2[i] = inp.nextInt();
 		System.out.print("Введите "+n+" значений для ввода в стек: ");
-		for(int i=0;i<n;i++)
+			for(int i=0;i<n;i++)
 		val3[i] = inp.nextInt();
-		me.push(val1);
-		me.push(val2);
-		me.push(val3);
-		me.show();
-		me.pop();
-		me.show();
-		me.pop();
-		me.show();
+		test.push(val1);
+		test.push(val2);
+		test.push(val3);
+		test.show();
+		test.pop();
+		test.show();
+		test.pop();
+		test.show();
 	}
 }
